173eae2e6a9e2f4973044083f9d17fad2ee6cbc9
kbake <kbake@outlook.com>
2013-12-15 14:00:35 -0500

    getting a lot of the gameplay stuff done

--- Gameplay.cpp
+++ Gameplay.cpp
@@ -3,8 +3,13 @@
 
 
 Gameplay::Gameplay(void) :
+	_beginRoundTick(0.f),
+	_inRoundTick(0.f),
+	_endRoundTick(0.f),
 	_roundNumber(0),
-	_gameplayState(RoundBegin)
+	_gameplayState(BeginRound),
+	_failed(false),
+	_quit(false)
 {
 	_font.loadFromFile("fonts/arial.ttf");
 
@@ -12,8 +17,19 @@ Gameplay::Gameplay(void) :
 	_roundNumberText.setPosition(6, 6);
 	_roundNumberText.setString("Round " + std::to_string(_roundNumber));
 
-	_ellipsisText.setFont(_font);
-	_ellipsisText.setPosition(518, 254);
+	_messageText.setFont(_font);
+	_messageText.setStyle(sf::Text::Italic);
+	_messageText.setCharacterSize(50);
+	_messageText.setPosition(260, 208);
+	_messageText.setString("Get ready");
+
+	_timeText.setFont(_font);
+	_timeText.setPosition(100, 100);
+	_timeText.setCharacterSize(80);
+
+	_textToDraw.push_back(&_timeText);
+	_textToDraw.push_back(&_roundNumberText);
+	_textToDraw.push_back(&_messageText);
 }
 
 
@@ -25,15 +41,78 @@ void Gameplay::Update(double deltaTime)
 {
 	switch(_gameplayState)
 	{
-	case RoundBegin:
+	case BeginRound:
+		_beginRoundTick += deltaTime;
+
+		if (_beginRoundTick >= 2.f)
+		{
+			_beginRoundTick = 0;
+
+			_textToDraw.erase(_textToDraw.begin()+1, _textToDraw.end());
+
+			_gameplayState = InRound;
+		}
+
 		break;
 	case InRound:
+		_inRoundTick += deltaTime;
+
+		if (_event.type == sf::Event::KeyPressed)
+		{
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			{
+				_quit = true;
+			}
+		}
+
+		if (_inRoundTick >= 1.f)
+		{
+			_failed = true;
+
+			_gameplayState = EndRound;
+		}
+
 		break;
-	case RoundEnd:
+	case EndRound:
+		_endRoundTick += deltaTime;
+
+		if (_textToDraw.size() < 2)
+		{
+			_textToDraw.push_back(&_roundNumberText);
+			_textToDraw.push_back(&_messageText);
+		}
+
+		if (_endRoundTick >= 2.f)
+		{
+			_endRoundTick = 0.f;
+
+			_gameplayState = BeginRound;
+		}
+
 		break;
 	}
 }
 
 void Gameplay::Draw(sf::RenderWindow& rw)
 {
+	std::vector<sf::Text*>::iterator end = _textToDraw.end();
+	for (std::vector<sf::Text*>::iterator it = _textToDraw.begin(); it != end; it++)
+	{
+		rw.draw(**(it));
+	}
+}
+
+void Gameplay::SetEvent(sf::Event& event)
+{
+	_event = event;
+}
+
+bool Gameplay::EndGame()
+{
+	if (_failed || _quit)
+	{
+		return true;
+	}
+
+	return false;
 }
--- Gameplay.h
+++ Gameplay.h
@@ -5,19 +5,31 @@ public:
 	Gameplay(void);
 	~Gameplay(void);
 
+	bool EndGame();
+	void SetEvent(sf::Event&);
+
 	void Update(double);
 	void Draw(sf::RenderWindow&);
 
 private:
-	enum GameplayState { RoundBegin, InRound, RoundEnd };
+	enum GameplayState { BeginRound, InRound, EndRound };
 
 	GameplayState _gameplayState;
 
+	sf::Event _event;
 	sf::Font _font;
 	sf::Text _roundNumberText;
-	sf::Text _ellipsisText;
+	sf::Text _messageText;
 	sf::Text _goText;
+	sf::Text _timeText;
 
+	std::vector<sf::Text*> _textToDraw;
+
+	float _beginRoundTick;
+	float _inRoundTick;	
+	float _endRoundTick;
 	unsigned int _roundNumber;
+	bool _failed;
+	bool _quit;
 };
 
